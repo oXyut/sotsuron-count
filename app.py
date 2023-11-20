1e685970421ba3c1e6652477821f253c370dd311
Yuto Suzuki <yuto.suzuki.0403@gmail.com>
2023-11-20 18:13:29 +0900

    fix:秒数表示をなくした

--- app.py
+++ app.py
@@ -23,7 +23,8 @@ def main():
     st.write('2024年1月9日までの残り日数を表示します。')
     today = datetime.datetime.today()
     # 何時何分何秒も含めて表示
-    st.write(f'今日は{today}です。')
+    # 例：2021年1月1日 12:34
+    st.write(f'現在時刻は{today:%Y年%m月%d日 %H時%M分}です。')
     remaining_days = (DEADLINE - today).days
     st.write(f'土日を含めてあと{remaining_days}日です。')
     # 表示
