3e48c607e64d5b4f9ba359a596ae0ee8e34aa7bb
Yuto Suzuki <yuto.suzuki.0403@gmail.com>
2023-11-20 18:18:18 +0900

    fix:日本時間が表示されるようにした

--- app.py
+++ app.py
@@ -4,8 +4,13 @@ import datetime
 
 DEADLINE = datetime.datetime(2024, 1, 9)
 
-def calc_remaining_days_without_weekend(today, remaining_days):
+def to_japan_time(dt: datetime.datetime):
+    # 日本時間に変換する
+    return dt + datetime.timedelta(hours=9)
+
+def calc_remaining_days_without_weekend(today:datetime.datetime, remaining_days:int) -> int:
     # 土日を除いた残りの日付を計算
+    today = to_japan_time(today)
     remaining_days_without_weekend = remaining_days
     for i in range(remaining_days):
         if today.weekday() in [5, 6]:
@@ -13,10 +18,11 @@ def calc_remaining_days_without_weekend(today, remaining_days):
         today += datetime.timedelta(days=1)
     return remaining_days_without_weekend
 
-def calc_remaining_days():
+def calc_remaining_days() -> int:
     today = datetime.datetime.today()
-    remaining_days = (DEADLINE - today).days
+    remaining_days = (to_japan_time(DEADLINE) - to_japan_time(today)).days
     return remaining_days
+
 
 def main():
     st.title('卒論の締め切りまでの残り日数')
@@ -24,8 +30,9 @@ def main():
     today = datetime.datetime.today()
     # 何時何分何秒も含めて表示
     # 例：2021年1月1日 12:34
-    st.write(f'現在時刻は{today:%Y年%m月%d日 %H時%M分}です。')
-    remaining_days = (DEADLINE - today).days
+    # 日本時間に変換する
+    st.write(f'現在時刻は{to_japan_time(today).strftime("%Y年%m月%d日 %H:%M:%S")}です。')
+    remaining_days = calc_remaining_days()
     st.write(f'土日を含めてあと{remaining_days}日です。')
     # 表示
     remaining_days_without_weekend = calc_remaining_days_without_weekend(today, remaining_days)
